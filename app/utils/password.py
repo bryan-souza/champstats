d39ba1ab4c14cec934e963c8e1add2482e74da6f
bryan-souza <bryans.souza@gmail.com>
2022-11-06 22:17:13 -0300

    refactor: PasswordService
    changes the way `hash_password` and `verify_password` handle bytes

--- app/utils/password.py
+++ app/utils/password.py
@@ -9,11 +9,12 @@ class PasswordService(metaclass=Singleton):
     def __init__(self):
         self._salt = CONFIG.salt
 
-    def hash_password(self, password: str) -> bytes:
-        b_password = bytes(password, encoding='UTF-8')
-        return bcrypt.hashpw(b_password, self._salt)
+    def hash_password(self, password: str) -> str:
+        b_password = bytes(password, encoding='utf-8')
+        h_password = bcrypt.hashpw(b_password, self._salt)
+        return h_password.decode(encoding='utf-8')
 
     def verify_password(self, password: str, hashed_password: str) -> bool:
-        b_password = bytes(password, encoding='UTF-8')
-        h_password = bytes(hashed_password, encoding='UTF-8')
+        b_password = bytes(password, encoding='utf-8')
+        h_password = bytes(hashed_password, encoding='utf-8')
         return bcrypt.checkpw(b_password, h_password)
