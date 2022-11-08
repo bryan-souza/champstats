50cbb35336b8ca7c2fb6f9c928da072d6ba042d1
bryan-souza <bryans.souza@gmail.com>
2022-11-08 14:23:27 -0300

    feat: added user-related functions
    implements user registering, email confirmation and `forgot password` mechanism

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,18 +1,29 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Body, Response
+from fastapi import status
 from fastapi_jwt_auth import AuthJWT
+from pydantic import EmailStr
 
 from app.models import UserAuth
 from app.controllers import AuthController
-from app.exceptions import NotFoundError
+from app.exceptions import NotFoundError, UserAlreadyExistsError, EmailAlreadyVerifiedError, AccountDisabledError
 
 router = APIRouter(prefix='/auth')
+
+
+@router.post('/register', status_code=status.HTTP_200_OK)
+async def register(user_auth: UserAuth, auth_controller: AuthController = Depends(AuthController)):
+    try:
+        return await auth_controller.register_new_user(user_auth)
+    except UserAlreadyExistsError:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail="User already exists")
 
 
 @router.post('/login', status_code=status.HTTP_200_OK)
 async def login(user_auth: UserAuth, auth: AuthJWT = Depends(),
                 auth_controller: AuthController = Depends(AuthController)):
     try:
-        return await auth_controller.login(user_auth, auth)
+        return await auth_controller.user_login(user_auth, auth)
     except NotFoundError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail='Bad credentials')
@@ -20,4 +31,32 @@ async def login(user_auth: UserAuth, auth: AuthJWT = Depends(),
 
 @router.post('/refresh', status_code=status.HTTP_200_OK)
 async def refresh(auth: AuthJWT = Depends(), auth_controller: AuthController = Depends(AuthController)):
-    return await auth_controller.refresh(auth)
+    return await auth_controller.refresh_access_token(auth)
+
+
+@router.post('/forgot-password', status_code=status.HTTP_200_OK)
+async def forgot_password(email: EmailStr = Body(..., embed=True), auth: AuthJWT = Depends(),
+                          auth_controller: AuthController = Depends(AuthController)):
+    try:
+        await auth_controller.forgot_password(email, auth)
+        return Response(status_code=status.HTTP_200_OK)
+    except EmailAlreadyVerifiedError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Email already verified")
+    except AccountDisabledError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Account disabled")
+
+
+@router.post('/reset-password/{token}', status_code=status.HTTP_200_OK)
+async def reset_password(token: str, new_password: str = Body(..., embed=True),
+                         auth: AuthJWT = Depends(),
+                         auth_controller: AuthController = Depends(AuthController)):
+    try:
+        return await auth_controller.reset_password(token, new_password, auth)
+    except EmailAlreadyVerifiedError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Email already verified")
+    except AccountDisabledError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Account disabled")
