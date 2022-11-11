5329094597e8a6b0774ddf4ebdcfab1f26bbb54c
bryan-souza <bryans.souza@gmail.com>
2022-11-11 19:31:00 -0300

    fix: moved jwt functions to server.py
    fixes error 500 when user tries to login

--- app/server.py
+++ app/server.py
@@ -1,6 +1,8 @@
 import os
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 
@@ -9,6 +11,16 @@ from app.routers import game, auth
 from app.config import CONFIG
 
 app = FastAPI()
+
+
+@AuthJWT.load_config
+def get_config():
+    return CONFIG
+
+
+@app.exception_handler(AuthJWTException)
+def jwt_exception_handler(request: Request, exc: AuthJWTException):
+    return JSONResponse(status_code=exc.status_code, content={"detail": exc.message})
 
 
 @app.on_event('startup')
