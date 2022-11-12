706521fcad0ae91fa943a5bced23bb2d25b2bd40
bryan-souza <bryans.souza@gmail.com>
2022-11-12 16:40:26 -0300

    feat: added synchronization
    implements synchronization upon insert or delete with `games` collection

--- app/routers/championship.py
+++ app/routers/championship.py
@@ -1,8 +1,9 @@
+from beanie import PydanticObjectId
 from fastapi import APIRouter, Depends, Response, status
 from fastapi_jwt_auth import AuthJWT
 
 from app.models import Championship
-from app.controllers import ChampionshipController
+from app.controllers import GameController, ChampionshipController
 from app.exceptions import NotFoundError
 
 router = APIRouter(prefix='/jogos/{game_id}/campeonatos')
@@ -17,12 +18,21 @@ async def get_all_championships(
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 async def insert_championship(
+        game_id,
         champ: Championship,
         championship_controller: ChampionshipController = Depends(ChampionshipController),
+        game_controller: GameController = Depends(GameController),
         auth: AuthJWT = Depends()
 ):
     auth.jwt_required()
-    return await championship_controller.insert(champ)
+    _champ = await championship_controller.insert(champ)
+
+    # HACK: Collections synchronization
+    _game = await game_controller.get_by_id(game_id)
+    _game.campeonatos.append(_champ.id)
+    await _game.save()
+
+    return _champ
 
 
 @router.get('/{champ_id}', status_code=status.HTTP_200_OK)
@@ -52,13 +62,20 @@ async def update_game(
 
 @router.delete('/{champ_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_game(
-        champ_id,
+        game_id, champ_id,
         championship_controller: ChampionshipController = Depends(ChampionshipController),
+        game_controller: GameController = Depends(GameController),
         auth: AuthJWT = Depends()
 ):
     auth.jwt_required()
     try:
         await championship_controller.delete(champ_id)
+
+        # HACK: Collections synchronization
+        _game = await game_controller.get_by_id(game_id)
+        _game.campeonatos.remove(PydanticObjectId(champ_id))
+        await _game.save()
+
     except NotFoundError:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
     else:
