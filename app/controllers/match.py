f0fb18e500e1c297f174099c42352dfd46fa338b
bryan-souza <bryans.souza@gmail.com>
2022-11-13 13:13:23 -0300

    feat: implemented sync
    implements synchronization with `championships` collection

--- app/controllers/match.py
+++ app/controllers/match.py
@@ -1,10 +1,12 @@
+from app.controllers import ChampionshipController
 from app.models import Match
 from app.exceptions import MatchNotFoundError
 
 
 class MatchController:
     def __init__(self):
-        ...
+        # HACK: Synchronization with championships collection
+        self._championship_controller: ChampionshipController = ChampionshipController()
 
     async def get_all(self):
         return await Match.find_all().to_list()
@@ -15,8 +17,12 @@ class MatchController:
             raise MatchNotFoundError(match_id)
         return _match
 
-    async def insert(self, match: Match):
-        return await Match.insert_one(match)
+    async def insert(self, champ_id: str, match: Match):
+        _match = await Match.insert_one(match)
+        _championship = await self._championship_controller.get_by_id(champ_id)
+        _championship.partidas.append(_match.id)
+        await self._championship_controller.update(champ_id, _championship)
+        return _match
 
     async def update(self, match_id, match: Match):
         _match = await Match.get(match_id)
@@ -26,8 +32,13 @@ class MatchController:
         await _match.set(match.dict(exclude_unset=True))
         return _match
 
-    async def delete(self, match_id):
+    async def delete(self, champ_id, match_id):
         _match = await Match.get(match_id)
         if not _match:
             raise MatchNotFoundError(match_id)
+
+        _championship = await self._championship_controller.get_by_id(champ_id)
+        _championship.partidas.remove(match_id)
+        await self._championship_controller.update(champ_id, _championship)
+
         return await _match.delete()
