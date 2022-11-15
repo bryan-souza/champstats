c0f90eb3653db44b116fa3cae3ab6effe0ddf937
bryan-souza <bryans.souza@gmail.com>
2022-11-15 00:48:07 -0300

    refactor: moved championship related models
    moves championship related models to its own submodule

---
+++ app/models/championship.py
@@ -0,0 +1,23 @@
+from beanie import Document, PydanticObjectId
+from typing import Optional, List
+from datetime import datetime
+from app.utils import encoders
+
+
+class Championship(Document):
+    nome: str
+    equipes: List[str]
+    vencedor: Optional[str] = None
+    premiacao: float
+    mvp: Optional[str] = None
+    local: str
+    lotacao: Optional[int] = None
+    datas: Optional[List[datetime]] = None
+    situacao: str
+    partidas: List[PydanticObjectId] = []
+
+    class Settings:
+        name = 'championships'
+        bson_encoders = {
+            datetime: encoders.datetime_encoder
+        }
