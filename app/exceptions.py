954f4e97b9bbd6fd5f0f2bdf6eb49399830a6ff9
bryan-souza <bryans.souza@gmail.com>
2022-11-17 22:07:59 -0300

    fix: fixed typing for `NotFoundError` parameters

--- app/exceptions.py
+++ app/exceptions.py
@@ -4,7 +4,7 @@ from typing import Optional
 class NotFoundError(Exception):
     entity_name: str
 
-    def __init__(self, entity_id: Optional[str] = None):
+    def __init__(self, entity_id=None):
         _message = f'{self.entity_name} not found'
         if entity_id:
             _message = f'{self.entity_name} not found, id: {entity_id}'
